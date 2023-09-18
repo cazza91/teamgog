c362beaac0192e7aa2e14c29b6aca07b43c0591e
Fabio Cazzavillan <fabiocazzavillan@gmail.com>
2023-09-18 21:53:18 +0200

    styled desktop header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -7,13 +7,15 @@ export const Header = component$(() => {
 
     const menu = MAIN_MENU;
 
-    return <header class={style.header}>
-        <nav>
-            <ul>
+    return <header class="shadow-md w-100">
+        <nav class="max-w-5xl py-4 flex items-center mx-auto">
+            <p>logo</p>
+            <ul class="grow flex gap-4 justify-center">
                 {menu.map((item, index) => {
                     return <li key={index + item.label}><Link href={item.url}>{item.label}</Link></li>
                 })}
             </ul>
+            <p>login</p>
         </nav>
     </header>
 })
