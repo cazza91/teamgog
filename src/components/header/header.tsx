14ee41c94c1448db81e3fe5e6f7573b6f02f97c7
Fabio Cazzavillan <fabiocazzavillan@gmail.com>
2023-09-21 22:52:57 +0200

    added some styling to hero section and header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,21 +1,31 @@
 import { component$ } from "@builder.io/qwik";
-import { Link } from "@builder.io/qwik-city";
 import { MAIN_MENU } from "~/shared/enums/mainMenu";
-import style from "./header.module.css";
+import { UnderlinedLink } from "../underlined-link/underlined-link";
 
 export const Header = component$(() => {
 
     const menu = MAIN_MENU;
 
     return <header class="shadow-md w-100">
-        <nav class="max-w-5xl py-4 flex items-center mx-auto">
+        <nav class="max-w-5xl h-20 py-4 flex items-center mx-auto">
             <p>logo</p>
             <ul class="grow flex gap-4 justify-center">
                 {menu.map((item, index) => {
-                    return <li key={index + item.label}><Link href={item.url}>{item.label}</Link></li>
+                    return <li key={index + item.label}>
+                              <UnderlinedLink href={item.url}>{item.label}</UnderlinedLink>
+                        </li>
                 })}
             </ul>
-            <p>login</p>
+            <button class="
+                border
+                border-gray-300
+                rounded-full
+                py-3 px-5
+                transition-all
+                hover:bg-gray-300
+                hover:-translate-y-1
+                hover:shadow-md
+            ">login</button>
         </nav>
     </header>
 })
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,9 +3,26 @@ import type { DocumentHead } from "@builder.io/qwik-city";
 
 export default component$(() => {
   return (
-    <>
-      <h1 class="text-center text-5xl font-bold">We are<br />Team GOG</h1>
-    </>
+    <section class="relative flex flex-col justify-center h-[32rem]">
+      <h3 class="text-center z-50">sentitamente vostro</h3>
+      <h1 class="text-center text-5xl font-bold z-50">We are<br />Team GOG</h1>
+      <article class="absolute left-0 right-0 top-0 bottom-0 bg-fuchsia-100 z-10">
+        <div class="
+          absolute
+          left-0
+          right-0
+          top-0
+          bottom-0
+          bg-gradient-to-r
+          from-blue-500
+          transition-all
+        "></div>
+        <div class="absolute inset-x-2/3 inset-y-1/3 w-24 h-24 rounded-full bg-fuchsia-400"></div>
+        <div class="w-24 h-24 rounded-full bg-fuchsia-400"></div>
+        <div class="w-24 h-24 rounded-full bg-fuchsia-400"></div>
+        <div class="w-24 h-24 rounded-full bg-fuchsia-400"></div>
+      </article>
+    </section>
   );
 });
 
@@ -14,7 +31,7 @@ export const head: DocumentHead = {
   meta: [
     {
       name: "description",
-      content: "Qwik site description",
+      content: "Team GOG description",
     },
   ],
 };
