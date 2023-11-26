21729445dbcd665e35488fa9eea1dfd74d36d463
Fabio Cazzavillan <fabiocazzavillan@gmail.com>
2023-11-26 10:37:00 +0100

    updated blog single layout

--- src/routes/blog/[url]/index.tsx
+++ src/routes/blog/[url]/index.tsx
@@ -1,7 +1,27 @@
 import { component$ } from "@builder.io/qwik";
 import { useLocation } from "@builder.io/qwik-city";
 
+import _blogsJson from '../../../data/blog.json';
+import type { IBlog } from "~/shared/interfaces/blog";
+
+
 export default component$(() => {
     const loc = useLocation();
-    return <div>Welcome to blog article {loc.params.url}!</div>;
+
+    const single: IBlog | undefined = (_blogsJson as IBlog[]).find(ev => ev.url === `/blog/${loc.params.url}`);
+
+    if (!single) return <></>;
+
+    return (
+        <section class="max-w-3xl mx-auto mt-10 px-4 gap-4">
+            <h1 class="text-center block w-full text-3xl font-bold mb-2">{single.title}</h1>
+            <p class="text-center block w-full text-gray-400 text-sm">
+                {new Date(single.pubblicationDate).toLocaleDateString("it-IT", { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' })}
+            </p>
+            <img width="16" height="9" class="w-full mt-4" src={single.image} alt={single.title} />
+            <p class="block w-full mt-8 leading-7 text-gray-100">
+                {single.content}
+            </p>
+        </section>
+    );
 })
--- src/shared/interfaces/blog.tsx
+++ src/shared/interfaces/blog.tsx
@@ -4,6 +4,7 @@ export interface IBlog {
   image?: string,
   coverEmoji?: string,
   date: Date | string, // importing from json date result as a string, didn't found anything else
+  pubblicationDate: Date | string, // importing from json date result as a string, didn't found anything else
   location: string,
   content: string,
   url: string,
